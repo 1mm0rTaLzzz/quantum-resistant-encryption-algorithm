31ab44629a5b9229ed0fb47ee4f6e2023d919735
zgeorgin <zgeorginn@gmail.com>
2023-11-17 00:06:25 +0300

    Test rewrited

--- GenerateVectorsTest.cpp
+++ GenerateVectorsTest.cpp
@@ -1,7 +1,7 @@
 #include "GenerateVectors.cpp"
 #include <iostream>
 
-void print_vector(std::vector<double> a)    
+void PrintVector(std::vector<double> a)    
 {
     for (double i : a)
         std::cout << i << " ";
@@ -13,9 +13,9 @@ int main()
     int dim = 10;   
     VectorPair conv_vectors = GenerateConvVectors(10);
     std::vector<double> a = conv_vectors.first, b = conv_vectors.second;
-    print_vector(a);
-    print_vector(b);
+    PrintVector(a);
+    PrintVector(b);
     VectorPair inconv_vectors = GenerateInconvVectors(PointerPair (&a, &b));
-    print_vector(inconv_vectors.first);
-    print_vector(inconv_vectors.second); 
+    PrintVector(inconv_vectors.first);
+    PrintVector(inconv_vectors.second); 
 }
