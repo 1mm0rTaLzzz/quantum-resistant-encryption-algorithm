900134d05a99c1c8656f5845c841d0354b5c674a
zgeorgin <zgeorginn@gmail.com>
2023-11-17 00:04:40 +0300

    Rewrited codestyle

--- GenerateVectors.cpp
+++ GenerateVectors.cpp
@@ -2,10 +2,10 @@
 #include <ctime>
 #include <random>
 
-typedef std::pair<std::vector<double>, std::vector<double>> vector_pair;
-typedef std::pair<std::vector<double>*, std::vector<double>*> pointer_pair;
+typedef std::pair<std::vector<double>, std::vector<double>> VectorPair;
+typedef std::pair<std::vector<double>*, std::vector<double>*> PointerPair;
 
-vector_pair generate_conv_vectors(int dimension, int max_cord_value = 1000, int signs_after_point = 3)
+VectorPair GenerateConvVectors(int dimension, int max_cord_value = 1000, int signs_after_point = 3)
 {
     std::vector<double> a(dimension), b(dimension);
     std::vector<bool> a_cord(dimension), b_cord(dimension);
@@ -26,11 +26,11 @@ vector_pair generate_conv_vectors(int dimension, int max_cord_value = 1000, int 
             b[i] = double(rand() % (max_cord_value*int(pow(10, signs_after_point)))) / pow(10, signs_after_point);   
     }
 
-    return vector_pair (a, b);
+    return VectorPair (a, b);
 }
 
 
-vector_pair generate_inconv_vectors(pointer_pair conv_vectors)
+VectorPair GenerateInconvVectors(PointerPair conv_vectors)
 {
     std::vector<double>* a = conv_vectors.first, *b = conv_vectors.second;
     srand(time(0));
@@ -47,5 +47,5 @@ vector_pair generate_inconv_vectors(pointer_pair conv_vectors)
         b_inconv[i] = mult_a_2 * a->operator[](i) + mult_b_2 * b->operator[](i);
     }
 
-    return vector_pair (a_inconv, b_inconv);
+    return VectorPair (a_inconv, b_inconv);
 }
--- GenerateVectorsTest.cpp
+++ GenerateVectorsTest.cpp
@@ -11,11 +11,11 @@ void print_vector(std::vector<double> a)
 int main()
 {
     int dim = 10;   
-    vector_pair conv_vectors = generate_conv_vectors(10);
+    VectorPair conv_vectors = GenerateConvVectors(10);
     std::vector<double> a = conv_vectors.first, b = conv_vectors.second;
     print_vector(a);
     print_vector(b);
-    vector_pair inconv_vectors = generate_inconv_vectors(pointer_pair (&a, &b));
+    VectorPair inconv_vectors = GenerateInconvVectors(PointerPair (&a, &b));
     print_vector(inconv_vectors.first);
     print_vector(inconv_vectors.second); 
 }
